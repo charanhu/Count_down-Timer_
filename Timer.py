b70aee74fd772e0ab64f3e237ac8728a720d70a6
charanhu1 <charanhumail@gmail.com>
2022-03-18 16:07:27 +0530

    Timer added

---
+++ Timer.py
@@ -0,0 +1,27 @@
+#Python Program to Timer
+import time
+import datetime
+import os
+import sys
+
+def main():
+    print("This is a timer")
+    print("Enter the time in seconds")
+    time = int(input())
+    print("Enter the message")
+    message = input()
+    print("Enter the file name")
+    fileName = input()
+    print("Enter the number of times to repeat")
+    repeat = int(input())
+    print("Enter the time interval in seconds")
+    interval = int(input())
+    for i in range(repeat):
+        print(message)
+        time.sleep(time)
+        print("Time is up")
+        time.sleep(interval)
+        os.system("clear")
+    print("End of program")
+
+    
